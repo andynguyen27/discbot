f8ba382df7c8155f4ca6362426f1afe53db0f059
andynguyen27 <120394492+andynguyen27@users.noreply.github.com>
2024-10-01 18:26:41 -0500

    syncing...

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import hashlib
 import aiohttp
 import json
 import uvicorn
-
+from cogs.commands import Commands
 
 load_dotenv(override=True)
 
@@ -24,8 +24,18 @@ LOG = logging.getLogger(__name__)
 
 intents = discord.Intents.all()
 molebot = commands.Bot(command_prefix = '!',intents=intents)
-
 delete_message = False # configure this 
+
+
+# def load():
+#     for filename in os.listdir('./cogs'):
+#         if filename.endswith('.py'):
+#             try:
+#                 # await molebot.add_cog(Commands(molebot))
+#                 molebot.load_extension(f'cogs.{filename[:-3]}')
+#                 print(f"Loading: {filename}")
+#             except Exception as e:
+#                 print(f"Failed to load extension {filename}: {e}")
 
 @molebot.event 
 async def on_ready():
@@ -34,9 +44,9 @@ async def on_ready():
     print("-----------------------------")
 
 
-@molebot.command()
-async def hello(ctx):
-    await ctx.send("Mom, Dad, I missed you")
+# @molebot.command()
+# async def hello(ctx):
+#     await ctx.send("Mom, Dad, I missed you")
 
 
 @molebot.event
@@ -56,23 +66,25 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("You don't have permission to run this command")
 
-@molebot.command()
-async def sfw(ctx):
-    sfwlink = 'https://cdn.discordapp.com/attachments/1285702416096039005/1285702797622378571/dugj2lh6f6051.png?ex=66eb3b8a&is=66e9ea0a&hm=18e720217950d34ad1382990f4dc8f4940a694888c244a452f210812a540b732&'
-    await ctx.send(sfwlink)
 
-@molebot.command()
-async def dadjoke(ctx):
-    url = "https://icanhazdadjoke.com/"
-    myheaders = {"Accept": "application/json"}
-    response = requests.get(url, headers=myheaders)
+# @molebot.command()
+# async def sfw(ctx):
+#     sfwlink = 'https://cdn.discordapp.com/attachments/1285702416096039005/1285702797622378571/dugj2lh6f6051.png?ex=66eb3b8a&is=66e9ea0a&hm=18e720217950d34ad1382990f4dc8f4940a694888c244a452f210812a540b732&'
+#     await ctx.send(sfwlink)
 
-    if not response.ok:
-        print(response)
-        LOG.error(f"There was an error: {response.reason}")
 
-    joke = response.json().get("joke")
-    await ctx.send(joke)
+# @molebot.command()
+# async def dadjoke(ctx):
+#     url = "https://icanhazdadjoke.com/"
+#     myheaders = {"Accept": "application/json"}
+#     response = requests.get(url, headers=myheaders)
+
+#     if not response.ok:
+#         print(response)
+#         LOG.error(f"There was an error: {response.reason}")
+
+#     joke = response.json().get("joke")
+#     await ctx.send(joke)
 
 
 @molebot.tree.command(name="tempconv", description="temperature conversion", guild=discord.Object(id=SERVER_ID))
@@ -95,10 +107,10 @@ async def tempconv(interaction: discord.Interaction, unit: app_commands.Choice[s
 # )
 
 
-@molebot.command()
-async def test(ctx):
-    giflink = 'https://tenor.com/view/football-gif-22736897'
-    await ctx.send(f"Man getting hit by football\n {giflink}")
+# @molebot.command()
+# async def test(ctx):
+#     giflink = 'https://tenor.com/view/football-gif-22736897'
+#     await ctx.send(f"Man getting hit by football\n {giflink}")
 
 
 async def download_file(url, file_name):
@@ -110,7 +122,7 @@ async def download_file(url, file_name):
                     if not chunk:
                         break
                     file.write(chunk)
-
+                    return file
 # async def check_virus_link(message_link):
     
 #     url = "https://www.virustotal.com/api/v3/urls"
@@ -185,21 +197,37 @@ async def check_virus_link(message_link):
         return 'balls'
 
     
-async def check_virus(file_path):
-    with open(file_path, 'rb') as file:
-        file_bytes = file.read()
-        file_hash = hashlib.sha256(file_bytes).hexdigest()
-
-    params = {'apikey' : VIRUSTOTALAPI, 'resource': file_hash}
-    response = requests.get(url, params=params)
-
+async def check_virus(file):
+    # with open(file_path, 'rb') as file:
+    #     file_bytes = file.read()
+    #     file_hash = hashlib.sha256(file_bytes).hexdigest()
+    url = "https://www.virustotal.com/api/v3/files"
+    print("Typeof File: ", type(file))
+    payload = { "file" : file }
+    # headers = {'apikey' : VIRUSTOTALAPI, 'resource': file_hash}
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/x-www-form-urlencoded",
+        "x-apikey": VIRUSTOTALAPI
+    }
+    print("peepee")
+    response = requests.post(url, data=payload, headers=headers)
+    print("Response.text: ",response.text)
     if response.status_code == 200:
+        response_json = response.json()
+        response_2 = requests.get(url=response_json['data']['links']['self'], headers=headers)
+        stats = response_json.get('data', {}).get('attributes', {}).get('stats', {})
+        # Load the JSON response from the result variable
+        print("response_2.text:", response_2.text)
+        print("stats: ", stats)
+        response = json.loads(response_2.text)
         json_response = response.json()
-        if json_response['response_code'] == 1:
-            positives = json_response['positives']
-            total = json_response['total']
-            return positives > 0, f"{positives}/{total} threat detected in this file."
-        return False, "Error occurred while scanning."
+        print("json_response.txt json_response.text")
+        # if json_response['response_code'] == 1:
+        #     positives = json_response['positives']
+        #     total = json_response['total']
+        #     return positives > 0, f"{positives}/{total} threat detected in this file."
+        # return False, "Error occurred while scanning."
 
 
 @molebot.event
@@ -226,18 +254,18 @@ async def on_message(message):
     else:   
         await molebot.process_commands(message)
 
-    for attachment in message.attachments:
-        if attachment.content_type.startswith('image/'):
-            return
+    # for attachment in message.attachments:
+    #     if attachment.content_type.startswith('image/'):
+    #         return
         
-    if message.attachments:
+    if message.attachments is not None:
         for attachment in message.attachments:
             print('jollibee')
             file_name = attachment.filename
             file_url = attachment.url
             print(f'${file_name} ${file_url}')
-            await download_file(file_url, file_name)
-            is_infected, result = await check_virus(file_name)
+            file = await download_file(file_url, file_name)
+            is_infected, result = await check_virus(file)
             if is_infected:
                 delete_message = True
                 await message.channel.send(f"<@{message.author.id}> Virus scan detected. {result}")
@@ -245,11 +273,21 @@ async def on_message(message):
                 await message.channel.send(f"<@{message.author.id}> File is clean. {result}")
             os.remove(file_name)
 
+
+async def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await molebot.load_extension(f"cogs.{filename[:-3]}")
+
+
+async def main():
+    async with molebot:
+        await load()
+        await molebot.start(MOLEBOTTOKEN)
     # if delete_message:
     #     await message.delete()
-
-molebot.run(MOLEBOTTOKEN)
-
+asyncio.run(main())
+# asyncio.run(main())
 if __name__ == "__main__":
     main()
     # uvicorn.run(app, port=8000, host="0.0.0.0")
